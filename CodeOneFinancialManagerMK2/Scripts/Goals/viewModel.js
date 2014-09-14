f35cccc2137542707db9708b76f04b255aa06f22
Andrew Filippini <afilippini@huskers.unl.edu>
2014-09-14 16:28:49 -0500

    added goal analysis

--- CodeOneFinancialManagerMK2/Scripts/Goals/viewModel.js
+++ CodeOneFinancialManagerMK2/Scripts/Goals/viewModel.js
@@ -3,6 +3,11 @@
     goalDescription: ko.observable(""),
     goalAmount: ko.observable(0),
     goalDate: ko.observable(""),
+    tempgoalDescription: ko.observable("A Disney Vacation"),
+    tempgoalAmount: ko.observable("$2,000"),
+    tempgoalSaved: ko.observable("$1,700"),
+    tempDate: ko.observable("11/2/2014"),
+    tempEstimate: ko.observable("10/1/2014"),
     newGoalClicked: function () {
         $('#modal-1').prop('checked', true);
     },
