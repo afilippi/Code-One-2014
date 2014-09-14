6c5659911a778205470e86281205fa8f5b02aae6
Jacob Heidelk <jheidelk@cse.unl.edu>
2014-09-14 02:17:10 -0500

    Sounds like someone's got a case of the Sunday's

---
+++ CodeOneFinancialManagerMK2/Scripts/Analysis/viewModel.js
@@ -0,0 +1,13 @@
+﻿var availableAccounts = [
+        { id: 1, accountName: 'Savings' },
+        { id: 2, accountName: 'IRA' },
+        { id: 3, accountName: 'Checking' }
+];
+
+var AnalysisViewModel = {
+    availableAccounts: availableAccounts,
+    selectedAccount: ko.observable(availableAccounts[0])
+
+
+};
+ko.applyBindings(AnalysisViewModel);
