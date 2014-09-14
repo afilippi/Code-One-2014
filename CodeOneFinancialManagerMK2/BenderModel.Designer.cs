ad75dafc191ba927c1e8490eeee2d45cbfec0eca
David Dropinski <david@dropinski.com>
2014-09-13 21:50:04 -0500

    I think I need to commit this stuff

--- CodeOneFinancialManagerMK2/BenderModel.Designer.cs
+++ CodeOneFinancialManagerMK2/BenderModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Jake\Documents\Code-One-2014\CodeOneFinancialManagerMK2\BenderModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\David\Documents\src\Code-One-2014\CodeOneFinancialManagerMK2\BenderModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
